22f518634ab1fefecd87e5dcb39affb5db83cb97
Hjollo <anders.vagenes@gmail.com>
2025-10-28 10:34:24 +0100

    Setup Ready
    Ready for development

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getFirestore, addDoc, collection, doc, setDoc, getDoc, getDocs, query, where, orderBy } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js"
+import { firebaseConfig } from "./firebaseConfig.js";
+
